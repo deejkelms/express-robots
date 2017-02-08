ed9a6c671560ad7db2332e12d52970bdfaafce96
Derek Kelmanson <dkelmanson@gmail.com>
2017-02-08 16:46:30 -0500

    added views and controllers

--- app/controllers/home_controller.js
+++ app/controllers/home_controller.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log("VISITED THE HOME PAGE")
+  res.redirect('/robots');
 });
 
 module.exports = router;
